d548b516b59261c2752a4b9c076f2f50a31f11db
Arthur Frank <frank.artur0303@gmail.com>
2015-08-03 15:02:59 +0100

    First test

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* AF: comment edited from git bash
+*/
+class Deck
+{
+	private $suits = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
+	private $cards = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
+	private $deck  = array();
+
+	public function __construct()
+	{
+		$this->setDeck();
+	}
+
+	private function setDeck()
+	{
+		foreach ($this->suits as $suit) {
+			$this->deck[$suit] = $this->cards;
+		}
+	}
+
+	public function getDeck($shuffle = false)
+	{
+		if ($shuffle) {
+			
+		}
+	}
+
+	public function shuffleDeck()
+	{
+
+	}
+}
